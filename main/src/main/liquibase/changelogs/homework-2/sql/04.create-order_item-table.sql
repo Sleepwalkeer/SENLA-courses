464d32510d1646f81383990fd8844dd9eb530b14
Sleepwalker <sleepwalker.slpwlkr@gmail.com>
2023-01-23 12:26:52 +0300

    excessive module removed

--- main/src/main/liquibase/changelogs/homework-2/sql/04.create-order_item-table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE order_item (
-     order_id integer,
-     item_id integer,
-     PRIMARY KEY (order_id, item_id)
-);
--- main/src/main/liquibase/changelogs/homework-2/sql/15.add-index-on-rent_order.customer_id.sql
+++
@@ -1,1 +0,0 @@
-CREATE index index_customer_id on rent_order (customer_id);
